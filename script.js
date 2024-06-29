77afe3fbaa6e57b44414b3889f1d5ef7dc7eb63f
Kamal-Ouiguemmane <ouiguemmanekamal@gmail.com>
2024-06-30 00:50:29 +0100

    commit

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-const synth = window.speechSynthesis;
+const textInput = document.getElementById('text-input');
+const voiceSelect = document.getElementById('voice-select');
 
+const synth = window.speechSynthesis;
+let voices;
 function createVoiceOptions() {
-  const voices = synth.getVoices();
+  voices = synth.getVoices();
 
   voices.forEach(voice => {
     const option = document.createElement('option');
-    option.textContent = `${voice.name} ${voice.lang}`;
-    option.setAttribute('data-');
-    document.getElementById('voice-select').appendChild(option);
+    option.textContent = `${voice.name} - ${voice.lang}`;
+    option.setAttribute('data-name', voice.name);
+    voiceSelect.appendChild(option);
   });
 }
 
 function onSubmit(e) {
   e.preventDefault();
 
-  const inputText = document.getElementById('text-input').value;
+  const input = textInput.value;
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute('data-name');
 
-  const utterance = new SpeechSynthesisUtterance(inputText);
+  const utterance = new SpeechSynthesisUtterance(input);
+  utterance.voice = voices.find(voice => voice.name === selectedOption);
+
+  synth.speak(utterance);
 }
 
 if (synth.onvoiceschanged !== undefined) {
